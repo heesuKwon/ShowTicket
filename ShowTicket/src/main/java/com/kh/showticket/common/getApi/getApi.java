380601540030c0e18314052f2713794a3a469dc6
kimjih114 <kimjih114@gmail.com>
2019-09-23 15:27:23 +0900

    getApi 주석처리

--- ShowTicket/src/main/java/com/kh/showticket/common/getApi/getApi.java
+++ ShowTicket/src/main/java/com/kh/showticket/common/getApi/getApi.java
@@ -20,72 +20,80 @@ import org.xml.sax.SAXException;
 
 public class getApi {
 	
-	
 	static DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	static DocumentBuilder documentBuilder;
 	static Document doc;
 	
-		public static String getTagValue(String tag, Element element) {
-			NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
-			Node value = (Node)nodeList.item(0);
-			if(value==null) {
-				return null;
-			}
-			return value.getNodeValue();
+	public static String getTagValue(String tag, Element element) {
+		NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+		Node value = (Node)nodeList.item(0);
+		
+		if(value==null) {
+			return null;
 		}
 		
-		public static List<Map<String, String>> getList(String url){
-			List<Map<String,String>> musicalList = new ArrayList<>();	
-			
-			try {
-				documentBuilder = factory.newDocumentBuilder();
-				doc = documentBuilder.parse(url);
-			} catch (ParserConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SAXException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			//root tag
-			doc.getDocumentElement().normalize();
-			//logger.debug("Root element: {} ", doc.getDocumentElement().getNodeName()); // Root element: dbs
+		return value.getNodeValue();
+	}
 		
-			NodeList nodeList = doc.getElementsByTagName("db");
-			//logger.debug("파싱할 리스트 수 : {}", nodeList.getLength());  // 파싱할 리스트 수 :  8
+	//공연목록조회
+	public static List<Map<String, String>> getList(String url){
+		List<Map<String,String>> musicalList = new ArrayList<>();	
+			
+		try {
+			documentBuilder = factory.newDocumentBuilder();
+			doc = documentBuilder.parse(url);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 			
 			
-			for(int i=0; i<nodeList.getLength(); i++){		
-				Node node = nodeList.item(i);
-				if(node.getNodeType() == Node.ELEMENT_NODE){
+		doc.getDocumentElement().normalize();
+		//logger.debug("Root element: {} ", doc.getDocumentElement().getNodeName()); // Root element: dbs
+		
+		NodeList nodeList = doc.getElementsByTagName("db");
+		//logger.debug("파싱할 리스트 수 : {}", nodeList.getLength());  // 파싱할 리스트 수 :  8
+			
+			
+		for(int i=0; i<nodeList.getLength(); i++){		
+			Node node = nodeList.item(i);
+			if(node.getNodeType() == Node.ELEMENT_NODE){
 									
-					Element element = (Element)node;
+			Element element = (Element)node;
 					
-					//logger.debug("=================================");
-					//logger.debug("공연ID: {}", getTagValue("mt20id", element));
-					//logger.debug("공연명: {}", getTagValue("prfnm", element));
-					//logger.debug("공연시작일: {}", getTagValue("prfpdfrom", element));
-					//logger.debug("공연종료일: {}", getTagValue("prfpdto", element));
-					//logger.debug("포스터이미지경로: {}", getTagValue("poster", element));
+			//logger.debug("=================================");
+			//logger.debug("공연ID: {}", getTagValue("mt20id", element));
+			//logger.debug("공연명: {}", getTagValue("prfnm", element));
+			//logger.debug("공연시작일: {}", getTagValue("prfpdfrom", element));
+			//logger.debug("공연종료일: {}", getTagValue("prfpdto", element));
+			//logger.debug("포스터이미지경로: {}", getTagValue("poster", element));
+			//logger.debug("공연장명 : {}", getTagValue("fcltynm", element));
 					
-					Map<String, String> map = new HashMap<>();
+			Map<String, String> map = new HashMap<>();
 					
-					map.put("mt20id", getTagValue("mt20id", element));
-					map.put("prfnm", getTagValue("prfnm", element));
-					map.put("prfpdfrom", getTagValue("prfpdfrom", element));
-					map.put("prfpdto", getTagValue("prfpdto", element));
-					map.put("poster", getTagValue("poster", element));
-					map.put("fcltynm", getTagValue("fcltynm", element));
-					musicalList.add(map);
+			//공연ID
+			map.put("mt20id", getTagValue("mt20id", element));
+			//공연명
+			map.put("prfnm", getTagValue("prfnm", element));
+			//공연시작일
+			map.put("prfpdfrom", getTagValue("prfpdfrom", element));
+			//공연종료일
+			map.put("prfpdto", getTagValue("prfpdto", element));
+			//포스터이미지경로
+			map.put("poster", getTagValue("poster", element));
+			//공연장명
+			map.put("fcltynm", getTagValue("fcltynm", element));
+					
+			musicalList.add(map);
 				
-				}
 			}
-			
-			return musicalList;
 		}
+			
+		return musicalList;
+		
+	}
 		
 }
